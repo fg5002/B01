0e0b96bcfde059140d23a15c8cd795c59b0fff83
Attila <attila.ocsag@gmail.com>
2024-11-14 10:57:36 +0100

    subGroup works already, markerCluster not yet 2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <div class="flex flex-col items-center h-screen p-12 font-bold bg-lime-300">
   <h1 class="text-2xl ">Welcome to FDC</h1>
   <div>An offline first PWA with OPFS, map and SQLite</div>
-  <div class="text-red-500 ">version 0.2</div>
+  <div class="text-red-500 ">version 0.2.1</div>
   
 </div>
